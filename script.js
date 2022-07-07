20ba5ca9f1fe55d44087ebbc656e1d7bb2ca399a
Nikolas Alexsander <55640359+NKLzT@users.noreply.github.com>
2022-07-07 18:46:36 -0300

    teste2

--- script.js
+++ script.js
@@ -1,54 +1,44 @@
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 const STAR_COUNT = 100
 let result = ""
 for(let i = 0; i < STAR_COUNT; i++){
-    result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
+  result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
 }
 console.log(result.substring(0, result.length - 1))
 /*
- --------------------------------------
+--------------------------------------
 */
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
+document.addEventListener('DOMContentLoaded',function(event){
+var dataText = [ "FULLSTACK", "HTML,CSS,JAVASCRIPT", "JAVA,C#,PYTHON", ""];
 
-const textArray = ["HTML", "CSS", "JAVASCRIPT", "PYTHON","JAVA","C#"];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
+function typeWriter(text, i, fnCallback) {
 
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
+  if (i < (text.length)) {
+
+   document.querySelector("p").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+    setTimeout(function() {
+      typeWriter(text, i + 1, fnCallback)
+    }, 100);
+  }
+  else if (typeof fnCallback == 'function') {
+    setTimeout(fnCallback, 700);
   }
 }
-
-function erase() {
-  if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
+ function StartTextAnimation(i) {
+   if (typeof dataText[i] == 'undefined'){
+      setTimeout(function() {
+        StartTextAnimation(0);
+      }, 20000);
+   }
+  if (i < dataText[i].length) {
+   typeWriter(dataText[i], 0, function(){
+     StartTextAnimation(i + 1);
+   });
   }
 }
-
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
+StartTextAnimation(0);
 });
